36b8f80afc45dfbd6c4e2290ec54d3921ae4274f
surkovrs <sxurkovrs@gmail.com>
2017-08-18 15:30:27 +0300

    Add sout

--- src/com/sbt/javaschool/rnd/Solution.java
+++ src/com/sbt/javaschool/rnd/Solution.java
@@ -3,5 +3,6 @@ package com.sbt.javaschool.rnd;
 public class Solution {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("I study java!!");
     }
 }
